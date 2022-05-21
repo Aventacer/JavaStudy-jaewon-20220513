7cfa0cdf2ec8a7ed719a299f3ab9bb06ec8d4dda
JAEWON-1070\jaewu <jaewun1@gmail.com>
2022-05-21 12:54:52 +0900

    메소드화

--- JavaStudy_jaewon/src/j07_메소드/Method_Menu.java
+++ JavaStudy_jaewon/src/j07_메소드/Method_Menu.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class Method_Menu {
 	
+	public static void main(String[] args) {
+		
+		boolean loopFlag = true;
+		
+		System.out.println("프로그램 시작");
+		while(loopFlag) {
+			char select = 0;
+			
+			select_Menu();
+			select = inp_select(select);
+		 
+			if(select == 'q') {
+				break;
+			}else if(select == '1') {
+				loopFlag = inside_menu(loopFlag, select);
+			}else if(select == '2') {
+				loopFlag = inside_menu(loopFlag, select);
+			}else if(select == '3') {
+				loopFlag = inside_menu(loopFlag, select);
+			}else { 
+				System.out.println("해당 명령은 보기에 없습니다. \n다시 입력하시오. ");
+			}
+			//System.out.println();
+		}
+		System.out.println("프로그램 종료");
+	}
+	
 	public static void select_Menu () {
 		System.out.println("[메뉴 선택]");
 		System.out.println("1. java");
@@ -19,98 +46,42 @@ public class Method_Menu {
 		sc.nextLine();
 		return select;
 	}
-
-	public static void main(String[] args) {
-		int i = 0, j = 0;
+	public static boolean inside_menu(boolean loopFlag, char select) {
+		Scanner sc = new Scanner(System.in);
 		
-		boolean loopFlag = true;
+		String 	s1 = "java",	
+				s2 = "python",
+				s3 = "javaScript",
+				menu = null;
+				
+		menu = select == '1' ? s1 
+				: select == '2' ? s2 
+						:select == '3' ? s3 : null;
 		
-		System.out.println("프로그램 시작");
-		while(loopFlag) {
-			char select = 0;
+		System.out.println("\n" + menu + " 를 선택함\n");
+		while(true) {
+			System.out.println("[ " + menu + " 메뉴 ]");
+			System.out.println("1. " + menu + " 란?");
+			System.out.println("b. 뒤로가기");
+			System.out.println("q. 프로그램 종료");
 			
-			select_Menu();
-			inp_select(select);
-			
-			if(select == 'q') {
+			select = inp_select(select);
+			System.out.println();
+			if(select == 'b') {
 				break;
-			}else if(select == '1') {
-				System.out.println("\njava를 선택함\n");
-				while(true) {
-					System.out.println("[ java 메뉴 ]");
-					System.out.println("1. java란?");
-					System.out.println("b. 뒤로가기");
-					System.out.println("q. 프로그램 종료");
-					
-					System.out.print("명령을 입력하시오 : ");
-					select = sc.next().charAt(0);
-					sc.nextLine();
-					System.out.println();
-					if(select == 'b') {
-						break;
-					}else if(select == 'q'){
-						loopFlag = false;
-						break;
-					}else if(select == '1'){
-						System.out.println("java란 ... 입니다");
-						System.out.println("계속하려면 아무키나 입력하라");
-						sc.nextLine();
-					}else { 
-						System.out.println("해당 명령은 보기에 없습니다. \n다시 입력하시오. ");
-					}
-				}
-			}else if(select == '2') {
-				System.out.println("\npython를 선택함\n");
-				while(true) {
-					System.out.println("[ python 메뉴 ]");
-					System.out.println("1. python이란?");
-					System.out.println("b. 뒤로가기");
-					
-					System.out.print("명령을 입력하시오 : ");
-					select = sc.next().charAt(0);
-					sc.nextLine();
-					System.out.println();
-					if(select == 'b') {
-						break;
-					}else if(select == '1'){
-						System.out.println("python이란 ... 입니다");
-						System.out.println("계속하려면 아무키나 입력하라");
-						sc.nextLine();
-					}else { 
-						System.out.println("해당 명령은 보기에 없습니다. \n다시 입력하시오. ");
-					}
-				}
-			}else if(select == '3') {
-				System.out.println("\njavaScript를 선택함\n");
-				while(true) {
-					System.out.println("[ javaScript 메뉴 ]");
-					System.out.println("1. javaScript란?");
-					System.out.println("b. 뒤로가기");
-					
-					System.out.print("명령을 입력하시오 : ");
-					select = sc.next().charAt(0);
-					sc.nextLine();
-					System.out.println();
-					if(select == 'b') {
-						break;
-					}else if(select == '1'){
-						System.out.println("javaScript란 ... 입니다");
-						System.out.println("계속하려면 아무키나 입력하라");
-						sc.nextLine();
-					}else { 
-						System.out.println("해당 명령은 보기에 없습니다. \n다시 입력하시오. ");
-					}
-				}
+			}else if(select == 'q'){
+				loopFlag = false;
+				return loopFlag;
+			}else if(select == '1'){
+				System.out.println(menu + "란 ... 입니다");
+				System.out.println("계속하려면 아무키나 입력하라");
+				sc.nextLine();
 			}else { 
 				System.out.println("해당 명령은 보기에 없습니다. \n다시 입력하시오. ");
 			}
-			//System.out.println();
 		}
 		
-		
-		System.out.println("프로그램 종료");
+		return loopFlag;
 	}
 	
-	//public static boolean checkFlag ( )
-
 }
